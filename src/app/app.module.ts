ed1a4b10bf4c6bf2b38192af503832123d8d82ec
Joseph Joyce <joej326@gmail.com>
2018-03-02 09:49:33 -0500

    working on routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
@@ -9,6 +10,13 @@ import { ItemComponent } from './item/item.component';
 import { StarWarsService } from 'app/star-wars.service';
 import { LogService } from 'app/log.service';
 import { CreateCharacterComponent } from './create-character/create-character.component';
+import { HeaderComponent } from './header/header.component';
+
+
+const routes = [
+  { path: '', component: TabsComponent },
+  { path: 'new-character', component: CreateCharacterComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +24,13 @@ import { CreateCharacterComponent } from './create-character/create-character.co
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent
+    CreateCharacterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   // note: services can only be injected into other services if they are provided in the app module
   providers: [StarWarsService, LogService],
